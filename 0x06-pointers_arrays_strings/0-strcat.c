f7c3ad877ba212d7517fdf5c8913e6e410683c8a
evertcolombia <evertescalante@gmail.com>
2019-10-19 13:53:34 -0500

    hotfix

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+/*This program concatenae two strings*/
+
+/**
+ * _strlen - show the lenght of a string
+ * @s: pointe to  pass the array
+ *
+ * Return: none
+ */
+int _strlen(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+        ++i;
+    return (i);
+}
+
+/**
+ * *_strcat - concatenate dos strings
+ * @dest: pointer to a string
+ * @src: pointer to the strign to concatenate
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+    int i, a, b;
+
+    a = _strlen(dest);
+    b = _strlen(src);
+
+    for (i = 0; i <= b; i++)
+        dest[a + i] = src[i];
+ 
+    return (dest);
+}
