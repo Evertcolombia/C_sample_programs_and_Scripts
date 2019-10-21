8ede3021a7c34184a767f2a64ab08646fc7e418f
evertcolombia <evertescalante@gmail.com>
2019-10-21 14:16:54 -0500

    hotfix

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,9 @@
+/**
+ * unction-like macro SUM(x, y) that computes 
+ * the sum of the numbers x and y.
+ */
+
+#ifndef _4_SUM_H_
+#define _4_SUM_H_
+#define SUM(x, y) ((x) + (y))
+#endif /* _4_SUM_H_ */
