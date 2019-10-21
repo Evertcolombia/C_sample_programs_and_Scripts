014547d68e5615e2000979bc638b1615f0d31590
evertcolombia <evertescalante@gmail.com>
2019-10-21 13:57:53 -0500

    hotfix

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+/*
+ * create a header file that defines a macro named SIZE 
+ * as an abbregcc -Wall -pedantic -Werror -Wextra 0-main.c -o aviation for the token 1024.
+ */
+
+
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+#define SIZE 1024
+#endif /* _0_OBJECT_LIKE_MACRO_H_ */ 
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,9 @@
+/**
+ * Create a header file that defines a macro named PI 
+ * as an abbreviation for the token 3.14159265359.
+ */
+
+#ifndef _1_PI_H_
+#define _1_PI_H_
+#define PI 3.14159265359
+#endif /* _1_PI_H_ */
