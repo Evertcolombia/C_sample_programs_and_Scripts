24a55b24d5ad341fbadb32f7ae55ed81a7884067
evertcolombia <evertescalante@gmail.com>
2019-12-03 21:14:15 -0500

    _strchr function that return the first occurrence of a char in a tring

--- 0x07-even_more_pointers/holberton.h
+++ 0x07-even_more_pointers/holberton.h
@@ -1,7 +1,11 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 
+#include <stdlib.h>
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+int _strlen(char *s);
 
 #endif
