25c4e47ec6076a0e0fcb5f06feeabca7069105e6
evertcolombia <evertescalante@gmail.com>
2020-01-03 11:52:44 -0500

    header files exmaple with a progrma

---
+++ monty-proofs/prog5/external.h
@@ -0,0 +1,16 @@
+/*
+** This header must not contain header guards (like <assert.h> must not).
+** Each time it is invoked, it redefines the macros EXTERN, INITIALIZE
+** based on whether macro DEFINE_VARIABLES is currently defined.
+*/
+
+#undef EXTERN
+#undef INITIALIZE
+
+#ifdef DEFINE_VARIABLES /*If the DEFINE VARIABLES macro is defined*/
+#define EXTERN 		  /* nothing*/
+#define INITIALIZE(...) = __VA_ARGS__
+#else
+#define EXTERN 		  extern
+#define INITIALIZE(...)   /*nothing*/
+#endif /* DEFINE_VARIABLES */
