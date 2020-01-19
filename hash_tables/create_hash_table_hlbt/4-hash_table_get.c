7a26cd28491bdd5cc30b267b37d77853b30d8b0a
evertcolombia <evertescalante@gmail.com>
2020-01-19 14:55:48 -0500

    hotfix and function 4-

--- hash_tables/create_hash_table_hlbt/4-hash_table_get.c
+++ hash_tables/create_hash_table_hlbt/4-hash_table_get.c
@@ -3,13 +3,22 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	/*function that retrieves a value associated with a key.*/
+	unsigned long int index = 0;
 
 	/*test if ht is empty*/
+	if (ht == NULL)
+		return (NULL);
 
 	/*hash th key and get an index*/
+	index = key_index((unsigned char *) key, ht->size);
 
 	/*if there are a node in this index
 	 * return the value that is in ths key*/
-
-	/*if not return null*/
+	while(ht->array[index])
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+	return (NULL);
 }
