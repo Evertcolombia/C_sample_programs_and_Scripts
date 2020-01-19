4f688440f75c10a2fe7efbafd36d053f0f855cfa
evertcolombia <evertescalante@gmail.com>
2020-01-19 17:23:57 -0500

    hotfix

---
+++ hash_tables/create_hash_table_hlbt/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i = 0;
+	hash_node_t *tmp = NULL, *tmp_del;
+
+	if (ht == NULL)
+		return;
+
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i];
+			while (tmp != NULL)
+			{
+				tmp_del = tmp;
+				tmp = tmp->next;
+				free(tmp_del->key);
+				free(tmp_del->value);
+				free(tmp_del);
+			}i++;
+		}
+		else
+		{
+			i++;
+			continue;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
