7afab16ade94090ffe5a19fec452b7ca58287f64
evertcolombia <evertescalante@gmail.com>
2021-03-06 06:33:00 +0000

    add function to get if file if EFL file or not

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,15 +5,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+
+void is_ELF64(unsigned char *e_ident);
 
 int main(int argc, char *argv[])
 {
 	int fd, numRead;
-	Elf64_Ehdr efl_h;
+	Elf64_Ehdr *efl_h;
 
 	if (argc != 2)
 	{
-		dprinf(STDERR_FILENO, "Usage: elf-header <ELF-file>\n");
+		dprintf(STDERR_FILENO, "Usage: elf-header <ELF-file>\n");
 		exit(97);
 	}
 
@@ -24,20 +28,36 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	efl_h = malloc(sizeof(efl_h));
-	if (efl == NULL)
+	efl_h = malloc(sizeof(Elf64_Ehdr));
+	if (efl_h == NULL)
 	{
 		dprintf(STDERR_FILENO, "Cant use malloc\n");
 		exit(100);
 	}
 
 	numRead = read(fd, efl_h, sizeof(efl_h));
-	if (numRead = -1)
+	if (numRead == -1)
 	{
 		free(efl_h);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(99);
 	}
 
+	is_ELF64(efl_h->e_ident);
 
+
+	return 0;
+}
+
+void is_ELF64(unsigned char *e_ident)
+{
+	/*printf("%s", (char *) e_ident);*/
+
+	if (!strncmp((char *) e_ident, "\177ELF", 4))
+		printf("ELF Header:\n");
+	else
+	{
+		dprintf(STDERR_FILENO, "Error: ELF mismatch \n");
+		exit(98);
+	}
 }
