0107333490e09e65c08e6fbe2ab24b726d0a761f
evertcolombia <evertescalante@gmail.com>
2021-03-06 08:31:51 +0000

    add class and data poxis functions to read_elf file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,6 +10,9 @@
 void mannage_error(char *msg, int code);
 void is_ELF64(unsigned char *e_ident);
 void mannage_magic(Elf64_Ehdr *header);
+void mannage_class(Elf64_Ehdr *header);
+void mannage_data(Elf64_Ehdr *header);
+
 
 int main(int argc, char *argv[])
 {
@@ -37,13 +40,15 @@ int main(int argc, char *argv[])
 
 	is_ELF64(efl_h->e_ident);
 	mannage_magic(efl_h);
+	mannage_class(efl_h);
+	mannage_data(efl_h);
 	return 0;
 }
 
 void is_ELF64(unsigned char *e_ident)
 {
 	if (!strncmp((char *) e_ident, "\177ELF", 4))
-		printf("ELF Header:\n");
+		dprintf(STDOUT_FILENO, "ELF Header:\n");
 	else
 		mannage_error("Error: ELF mismatch \n", 98);
 }
@@ -53,12 +58,45 @@ void mannage_magic(Elf64_Ehdr *header)
 {
 	int i = 0;
 
-	printf("  Magic:   ");
+	dprintf(STDOUT_FILENO, "  Magic:   ");
 	for (i = 0; i < (EI_NIDENT - 1); i++)
-		printf("%02x ", header->e_ident[i]);
-	printf("%02x\n", header->e_ident[i]);
+		dprintf(STDOUT_FILENO, "%02x ", header->e_ident[i]);
+	dprintf(STDOUT_FILENO, "%02x\n", header->e_ident[i]);
 }
 
+void mannage_class(Elf64_Ehdr *header)
+{
+	dprintf(STDOUT_FILENO, "  Class:\t\t\t     ");
+	
+	switch(header->e_ident[EI_CLASS])
+	{
+		case ELFCLASS64:
+			dprintf(STDOUT_FILENO, "ELF64\n");
+			break;
+		case ELFCLASS32:
+			dprintf(STDOUT_FILENO, "ELF32\n");
+			break;
+		default:
+			dprintf(STDOUT_FILENO, "Invalid CLASS\n");
+			break;
+	}
+}
+
+void mannage_data(Elf64_Ehdr *header)
+{
+	dprintf(STDOUT_FILENO, "  Data:\t\t\t\t     ");
+	switch(header->e_ident[EI_DATA])
+	{
+		case ELFDATA2MSB:
+			dprintf(STDOUT_FILENO, "2's complement, big endian\n");
+			break;
+		case ELFDATA2LSB:
+			dprintf(STDOUT_FILENO, "2's complement, little endian\n");
+			break;
+		default:
+			dprintf(STDOUT_FILENO, "Invalid format %x\n", header->e_ident[EI_DATA]);
+	}
+}
 
 void mannage_error(char *msg, int code)
 {
