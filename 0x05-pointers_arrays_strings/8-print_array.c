5a439f9650c1139ac6f2b271a5f7f580b69bc43a
evertcolombia <evertescalante@gmail.com>
2021-03-21 21:49:27 +0000

    hotfix

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_array - print n element of array
+ * @a: int pointer to an array
+ * @n: number o elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 1; i < n && (*a + 1) != '\0'; i++)
+		printf("%i, ", *a++);
+	printf("%i\n", *a);
+}
